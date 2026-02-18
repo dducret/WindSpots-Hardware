b90948dc25d14f9f56bcf65daa99235f8465fedc
Denis Ducret <denis.ducret@sdic.ch>
2026-02-18 09:48:03 +0100

    Delete linux/opt/windspots/bin/cpp/w3ble directory

--- linux/opt/windspots/bin/cpp/w3ble/version.h
+++
@@ -1,8 +0,0 @@
-// version.h
-#ifndef VERSION_H_
-#define VERSION_H_
-#define W3BLE_VERSION_MAJOR 0
-#define W3BLE_VERSION_MINOR 1
-#define W3BLE_VERSION_BRANCH	"beta"
-extern char W3BLE_BUILD_DATE;
-#endif 
