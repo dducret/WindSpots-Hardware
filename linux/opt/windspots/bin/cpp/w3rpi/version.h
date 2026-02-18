5945aa8d1fd6838720b4c3b55b72270186d737ef
Denis Ducret <denis.ducret@sdic.ch>
2026-02-18 10:56:04 +0100

    Delete linux/opt/windspots/bin/cpp/w3rpi directory

--- linux/opt/windspots/bin/cpp/w3rpi/version.h
+++
@@ -1,8 +0,0 @@
-// version.h
-#ifndef VERSION_H_
-#define VERSION_H_
-#define W3RPI_VERSION_MAJOR 0
-#define W3RPI_VERSION_MINOR 3
-#define W3RPI_VERSION_BRANCH	"prod"
-extern char W3RPI_BUILD_DATE;
-#endif 
