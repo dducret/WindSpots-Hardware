8a023fbdca3e51b4d0b25c18e31c638057c1f34d
Denis Ducret <denis.ducret@sdic.ch>
2026-02-18 14:34:01 +0100

    Update upload_station.php

--- linux/opt/windspots/bin/php/upload_station.php
+++ linux/opt/windspots/bin/php/upload_station.php
@@ -1,75 +1,79 @@
 <?php
-$stationName = (isset($argv[1])) ? $argv[1] : "CHGE99";
-$displayName = (isset($argv[2])) ? $argv[2] : "Station Dev";
-$shortName = (isset($argv[3])) ? $argv[3] : "Station Dev";
-$MSName = (isset($argv[4])) ? $argv[4] : "NA";
-$information = (isset($argv[5])) ? $argv[5] : "WindSpots Dev - Please contact Denis";
-$spotType = (isset($argv[6])) ? $argv[6] : "4";
-$online = (isset($argv[7])) ? $argv[7] : "Y";
-$maintenance = (isset($argv[8])) ? $argv[8] : "N";
-$reason = (isset($argv[9])) ? $argv[9] : "NA";
-$altitude = (isset($argv[10])) ? $argv[10] : "376";
-$latitude = (isset($argv[11])) ? $argv[11] : "46.20199";
-$longitude = (isset($argv[12])) ? $argv[12] : "6.15921";
-$gmt = (isset($argv[13])) ? $argv[13] : "1";
-$url = (isset($argv[14])) ? $argv[14] : "http://station.windspots.com";
-$windspotsLog =  (isset($argv[15])) ? $argv[15] : "/var/log";
-//
+// Improved station registration script.
+
+// Retrieve command line arguments with defaults.
+$stationName  = $argv[1]  ?? "CHGE99";
+$displayName  = $argv[2]  ?? "Station Dev";
+$shortName    = $argv[3]  ?? "Station Dev";
+$MSName       = $argv[4]  ?? "NA";
+$information  = $argv[5]  ?? "WindSpots Dev - Please contact Denis";
+$spotType     = $argv[6]  ?? "4";
+$online       = $argv[7]  ?? "Y";
+$maintenance  = $argv[8]  ?? "N";
+$reason       = $argv[9]  ?? "NA";
+$altitude     = $argv[10] ?? "376";
+$latitude     = $argv[11] ?? "46.20199";
+$longitude    = $argv[12] ?? "6.15921";
+$gmt          = $argv[13] ?? "1";
+$url          = rtrim($argv[14] ?? "http://station.windspots.com", '/');
+$windspotsLog = $argv[15] ?? "/var/log";
+
 $registerURL = $url . "/register.php";
-$wscheck="ws100";
-//
-function microtime_float() {  
-  list($usec, $sec) = explode(" ", microtime());
-  return ((float)$usec + (float)$sec);
-}
-//
+$wscheck = "ws100";
+
+// Logging function.
 function logIt($message) {
-  global $windspotsLog;
-  $dirname = dirname(__FILE__);
-  $wlHandle = fopen($windspotsLog."/windspots.log", "a");
-  $t = microtime(true);
-  $micro = sprintf("%06d",($t - floor($t)) * 1000000);
-  $micro = substr($micro,0,3);
-  fwrite($wlHandle, Date("H:i:s").".$micro"." upload_station.php - ".$message."\n");
-  fclose($wlHandle);
+    global $windspotsLog;
+    $logFile = $windspotsLog . "/windspots.log";
+    $time = microtime(true);
+    $micro = sprintf("%03d", ($time - floor($time)) * 1000);
+    $logMessage = date("H:i:s") . ".$micro upload_station.php - $message" . PHP_EOL;
+    file_put_contents($logFile, $logMessage, FILE_APPEND | LOCK_EX);
 }
-//
-$current_date = Date("YmdHis");
-if($online == "N")
-	$online = 0;
-else
-	$online = 1;
-if($maintenance == "N")
-	$maintenance = 0;
-else
-	$maintenance = 1;
-// station 
-$data = array('station' => $stationName, 'date' => $current_date, 'stationName' => $stationName, 'displayName' => $displayName, 
-                  'shortName' => $shortName, 'MSName' => $MSName, 'information' => $information,
-                  'spotType' => $spotType, 'online' => $online, 'maintenance' => $maintenance, 'reason' => $reason, 
-                  'altitude' => $altitude, 'latitude' => $latitude, 'longitude' => $longitude, 'gmt' => $gmt,
-                  'check' => $wscheck);
-// upload data
-//logIt("Station: ".$stationName);
-//logIt("Date:    ".$current_date);
-//logIt("Data:    ".json_encode($data));
-//logIt("Data:    ".json_encode($stationData));
-$headers = array();
-$headers[] = "Accept: application/json";
-$headers[] = "Content-Type: application/x-www-form-urlencoded";
-$headers[] = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $registerURL);
+
+$current_date = date("YmdHis");
+
+// Normalize online and maintenance flags.
+$online = ($online === "N") ? 0 : 1;
+$maintenance = ($maintenance === "N") ? 0 : 1;
+
+// Build the data array for registration.
+$data = [
+    'station'     => $stationName,
+    'date'        => $current_date,
+    'stationName' => $stationName,
+    'displayName' => $displayName,
+    'shortName'   => $shortName,
+    'MSName'      => $MSName,
+    'information' => $information,
+    'spotType'    => $spotType,
+    'online'      => $online,
+    'maintenance' => $maintenance,
+    'reason'      => $reason,
+    'altitude'    => $altitude,
+    'latitude'    => $latitude,
+    'longitude'   => $longitude,
+    'gmt'         => $gmt,
+    'check'       => $wscheck
+];
+
+// Send registration data using cURL.
+$headers = [
+    "Accept: application/json",
+    "Content-Type: application/x-www-form-urlencoded",
+    "User-Agent: Mozilla/5.0"
+];
+$ch = curl_init($registerURL);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
-  logIt("Upload Data Error: ".curl_error($ch));
-  logIt("                   ".$result);
+    logIt("cURL error: " . curl_error($ch));
+    logIt("Response: " . $result);
 } else {
-  logIt("Transmit done for: ".$registerURL);
+    logIt("Registration successful for: $registerURL");
 }
-curl_close ($ch);
+curl_close($ch);
 ?>
