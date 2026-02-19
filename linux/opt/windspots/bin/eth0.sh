5a5d8a6fc66456a810e9c9baeee3235e679ce293
Denis Ducret <denis.ducret@sdic.ch>
2026-02-19 16:16:22 +0100

    Update eth0.sh
    V10.0

--- linux/opt/windspots/bin/eth0.sh
+++ linux/opt/windspots/bin/eth0.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-. `dirname $0`/common.sh        # common windspots scripts
-ws_log "request RJ45 $1"
-STATUS=`cat /sys/class/net/eth0/operstate`
-PARAMETER="$1"
-if [[ ${STATUS:0:2} == "up" ]];then
-  if [[ ${PARAMETER} == "down" ]];then
-     sudo ip link set eth0 down
-     ws_log "RJ45 going down..."
+. "$(dirname "$0")/common.sh"  # common windspots scripts
+
+ws_log "Request RJ45 $1"
+status=$(cat /sys/class/net/eth0/operstate)
+param="$1"
+
+if [ "${status%% *}" = "up" ]; then
+  if [ "$param" = "down" ]; then
+    sudo ip link set eth0 down
+    ws_log "RJ45 going down..."
   else
-     ws_log "RJ45 is already up."
+    ws_log "RJ45 is already up."
   fi
 else
-  if [[ ${PARAMETER} == "up" ]];then
-     sudo ip link set eth0 up
-     ws_log "RJ45 going up..."
+  if [ "$param" = "up" ]; then
+    sudo ip link set eth0 up
+    ws_log "RJ45 going up..."
   else
-     ws_log "RJ45 is already down."
+    ws_log "RJ45 is already down."
   fi
 fi
