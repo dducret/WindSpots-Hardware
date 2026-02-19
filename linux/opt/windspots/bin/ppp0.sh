46605004bffc0e77263049118e26ed5d2b83ad72
Denis Ducret <denis.ducret@sdic.ch>
2026-02-19 16:22:15 +0100

    Update ppp0.sh
    V10.0

--- linux/opt/windspots/bin/ppp0.sh
+++ linux/opt/windspots/bin/ppp0.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-. `dirname $0`/common.sh        # common windspots scripts
-ws_log "request 3G $1"
-STATUS=/sys/class/net/eth1/operstate
-PARAMETER="$1"
-if [[ ${STATUS:0:2} == "up" ]];then
-  if [[ ${PARAMETER} == "down" ]];then
-     sudo ip link set eth1 down
-     ws_log "3G going down..."
+. "$(dirname "$0")/common.sh"  # common windspots scripts
+
+ws_log "Request 3G $1"
+status=$(cat /sys/class/net/eth1/operstate)
+param="$1"
+
+if [ "${status%% *}" = "up" ]; then
+  if [ "$param" = "down" ]; then
+    sudo ip link set eth1 down
+    ws_log "3G going down..."
   else
-     ws_log "3G is already up."
+    ws_log "3G is already up."
   fi
 else
-  if [[ ${PARAMETER} == "up" ]];then
-     sudo ip link set eth1 up
-     ws_log "3G going up..."
+  if [ "$param" = "up" ]; then
+    sudo ip link set eth1 up
+    ws_log "3G going up..."
   else
-     ws_log "3G is already down."
+    ws_log "3G is already down."
   fi
 fi
