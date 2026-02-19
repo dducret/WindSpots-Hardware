b332f191d77fc55c8f978dbdc27583b198e20102
Denis Ducret <denis.ducret@sdic.ch>
2026-02-19 16:32:00 +0100

    Update reboot.sh
    V10.0

--- linux/opt/windspots/bin/reboot.sh
+++ linux/opt/windspots/bin/reboot.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-. `dirname $0`/common.sh        # common windspots scripts
-UPTIME="$(cat /proc/uptime | grep -o '^[0-9]\+')"
-if [ "$UPTIME" -le "300" ]; then
-  ws_log "Reboot postponed - uptime : $UPTIME"
-  ws_syslog "Reboot postponed - uptime : $UPTIME"
+. "$(dirname "$0")/common.sh"  # common windspots scripts
+
+# Get uptime in seconds (integer)
+UPTIME=$(cut -d. -f1 /proc/uptime)
+if [ "$UPTIME" -le 300 ]; then
+  ws_log "Reboot postponed - uptime: ${UPTIME}"
+  ws_syslog "Reboot postponed - uptime: ${UPTIME}"
   exit 1
 fi
-rm -v ${TMP}/lastconnection
-ws_log "Reboot....\n\n\n"
-UPMIN=$(( UPTIME/60%60 ))
-UPHOUR=$(( UPTIME/60/60%24 ))
-UPDAY=$(( UPTIME/60/60/24 ))
-ws_syslog "Reboot - Uptime: $UPDAY day(s) $UPHOUR:$UPMIN...\n"
+
+rm -v "${TMP}/lastconnection"
+ws_log "Reboot...."
+
+UPMIN=$(( (UPTIME / 60) % 60 ))
+UPHOUR=$(( (UPTIME / 3600) % 24 ))
+UPDAY=$(( UPTIME / 86400 ))
+ws_syslog "Reboot - Uptime: ${UPDAY} day(s) ${UPHOUR}:${UPMIN}..."
 /sbin/reboot
