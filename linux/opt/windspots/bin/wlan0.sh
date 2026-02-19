ab0a8af92d1f152413eaf527854c6945dd666dd7
Denis Ducret <denis.ducret@sdic.ch>
2026-02-19 16:32:33 +0100

    Update wlan0.sh
    V10.0

--- linux/opt/windspots/bin/wlan0.sh
+++ linux/opt/windspots/bin/wlan0.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-. `dirname $0`/common.sh        # common windspots scripts
-ws_log "request Wifi $1"
-STATUS=`cat /sys/class/net/wlan0/operstate`
+. "$(dirname "$0")/common.sh"  # Load common windspots scripts
+
+ws_log "Request Wifi $1"
+STATUS=$(cat /sys/class/net/wlan0/operstate)
 PARAMETER="$1"
-if [[ ${STATUS:0:2} == "up" ]];then
-  if [[ ${PARAMETER} == "down" ]];then
-     sudo ip link set wlan0 down
-     ws_log "Wifi going down..."
+
+if [[ "$STATUS" == up* ]]; then
+  if [[ "$PARAMETER" == "down" ]]; then
+    sudo ip link set wlan0 down
+    ws_log "Wifi going down..."
   else
-     ws_log "Wifi is already up."
+    ws_log "Wifi is already up."
   fi
 else
-  if [[ ${PARAMETER} == "up" ]];then
-     sudo ip link set wlan0 up
-     ws_log "Wifi going up..."
+  if [[ "$PARAMETER" == "up" ]]; then
+    sudo ip link set wlan0 up
+    ws_log "Wifi going up..."
   else
-     ws_log "Wifi is already down."
+    ws_log "Wifi is already down."
   fi
 fi
